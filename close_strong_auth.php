afc655b7a66785efb919f136c167f283c6440f21
Matteo Foderà <matteo.maurizio@leetchi.com>
2013-07-30 19:12:27 +0200

    Revert "gestion beneficiary"
    This reverts commit 7949b601e37bea969b8e21d6bf12190dc18c58a8.

--- close_strong_auth.php
+++ close_strong_auth.php
@@ -9,20 +9,14 @@
 require_once (dirname(__FILE__) . "/lib/common.inc");
 
 $user_id = isset($_REQUEST["user_id"]) ? $_REQUEST["user_id"] : 0;
-$beneficiary_id = isset($_REQUEST["beneficiary_id"]) ? $_REQUEST["beneficiary_id"] : 0;
 
-$Id_object = isset($_REQUEST["user_id"]) ? $_REQUEST["user_id"] : $beneficiary_id;
-
-
-if ($Id_object == 0) {
-	print("Error : not parameter id in url");	
-    return;
+if ($user_id == 0) {
+	print("Error : not parameter user_id in url");
+	return;
 }
 
-
-
 $body = json_encode(array("IsDocumentsTransmitted" =>  true));
-$strongUserValidation = request("users/".$Id_object ."/strongAuthentication", "PUT", $body);
+$strongUserValidation = request("users/".$user_id ."/strongAuthentication", "PUT", $body);
 
 ?>
 
